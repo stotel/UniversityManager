1f5f10d2b81943a673e8d7449a0ac2cb44f62d64
Vanya <oopsgu2006@gmail.com>
2024-03-03 11:05:58 +0200

    Initial faculty, department, professor and students. full task 1

--- src/NaUKMA.java
+++ src/NaUKMA.java
@@ -1,13 +1,37 @@
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class NaUKMA {
+
     private Faculty[] faculties = new Faculty[0];
     private Department[] departments;
-    public Faculty[] getFaculties(){
+
+    {
+        addFaculty(new Faculty("ФІ"));
+        Department depInit = new Department("Кафедра інформатики");
+        Professor prfInit = new Professor("Кирієнко", "Оксана");
+        Student stdInit1 = new Student(1, 5, "Гущін", "Іван");
+        Student stdInit2 = new Student(1, 5, "Стецик", "Максим");
+        Student stdInit3 = new Student(1, 5, "Рублевський", "Орест");
+
+        faculties[0].addDepartment(depInit);
+        faculties[0].addProfessor(prfInit);
+        faculties[0].addStudent(stdInit1);
+        faculties[0].addStudent(stdInit2);
+        faculties[0].addStudent(stdInit3);
+
+        depInit.addProfessor(prfInit);
+        depInit.addStudent(stdInit1);
+        depInit.addStudent(stdInit2);
+        depInit.addStudent(stdInit3);
+    }
+
+    public Faculty[] getFaculties() {
         return faculties;
     }
-    public Department[] getDepartments(){
+
+    public Department[] getDepartments() {
         int len = 0;
         Department[] deps = new Department[len];
         for (Faculty f : faculties) {
@@ -18,22 +42,26 @@ public class NaUKMA {
         departments = deps;
         return departments;
     }
+
     public void addFaculty(Faculty faculty) {
-        faculties = Arrays.copyOf(faculties,faculties.length+1);
-        faculties[faculties.length-1]=faculty;
+        faculties = Arrays.copyOf(faculties, faculties.length + 1);
+        faculties[faculties.length - 1] = faculty;
     }
+
     public void addDepartment(Department department, String facName) {
         Faculty faculty = findFaculty(facName);
-        if(faculty != null)
+        if (faculty != null)
             faculty.addDepartment(department);
         else
-            System.out.println("No faculty found, department was not added...");
+            System.out.println("Факультету не знайдено - кафедру не додано...");
     }
-    public void addStudent(Student student, String facName){
+
+    public void addStudent(Student student, String facName) {
         findFaculty(facName).addStudent(student);
     }
-    public void addProfessor(Professor professor, String depName){
-        for(Faculty f: faculties) {
+
+    public void addProfessor(Professor professor, String depName) {
+        for (Faculty f : faculties) {
             Department dep = f.findDepartment(depName);
             if (dep != null) {
                 dep.addProfessor(professor);
@@ -41,32 +69,68 @@ public class NaUKMA {
             }
         }
     }
+
     public void removeFaculty(String name) {
         Faculty f = getFaculty(name);
         if (f != null)
             Utils.remove(f, faculties);
+        else {
+            System.out.println("Такого факультету не існує");
+        }
     }
+
     public void removeDepartment(String name) {
         Department d = getDepartment(name);
-        if(d != null){
+        if (d != null) {
             d.getFaculty().removeDepartment(d);
         }
     }
-    public void removeStudent(String name, String sname){
+
+    public void removeStudent(String name, String sname) {
         Student s = getStudent(name, sname);
-        if(s != null)
+        if (s != null)
             s.getFaculty().removeStudent(s);
     }
-    public void removeProfessor(String name, String sname){
+
+    public void removeProfessor(String name, String sname) {
         Professor p = getProfessor(name, sname);
         if (p != null)
             p.getDepartment().removeProfessor(p);
     }
-    public Faculty getFaculty(String name){
+
+    public void changeFaculty(Faculty faculty, String newName) {
+        if (findFaculty(faculty.getName()) == null) {
+            faculty.setName(newName);
+        } else {
+            System.out.println("Факультет з такою назвою вже існує");
+        }
+    }
+
+    public void facultyActions() throws IOException {
+        int choice = DataInput.getInt("Оберіть дію з факультетом: \n1. Створити \n2. Видалити \n3. Редагувати \n");
+        switch (choice) {
+            case 1:
+                addFaculty(new Faculty(DataInput.getString("Введіть назву нового факультету: ")));
+                break;
+            case 2:
+                removeFaculty(DataInput.getString("Введіть назву факультету для видалення: "));
+                break;
+            case 3:
+                String oldName = DataInput.getString("Введіть стару назву факультету: ");
+                String newName = DataInput.getString("Введіть нову назву факультету: ");
+                Faculty faculty = findFaculty(oldName);
+                if (faculty != null) {
+                    changeFaculty(faculty, newName);
+                }
+        }
+    }
+
+    public Faculty getFaculty(String name) {
         return findFaculty(name);
     }
-    public Department getDepartment(String name){
-        for(Faculty f: faculties) {
+
+    public Department getDepartment(String name) {
+        for (Faculty f : faculties) {
             Department d = f.findDepartment(name);
             if (d != null) {
                 return d;
@@ -75,8 +139,8 @@ public class NaUKMA {
         return null;
     }
 
-    public Student getStudent(String name, String sname){
-        for(Faculty f: faculties) {
+    public Student getStudent(String name, String sname) {
+        for (Faculty f : faculties) {
             Student s = f.findStudent(name, sname);
             if (s != null) {
                 return s;
@@ -84,21 +148,23 @@ public class NaUKMA {
         }
         return null;
     }
+
     //todo
-    public Student getStudent(int group, boolean courseInstead){
-        for(Faculty f: faculties) {
-            Student s = (courseInstead)? null:null;
+    public Student getStudent(int group, boolean courseInstead) {
+        for (Faculty f : faculties) {
+            Student s = (courseInstead) ? null : null;
             if (s != null) {
                 return s;
             }
         }
         return null;
     }
-    public Professor getProfessor(String name, String sname){
-        for(Faculty f: faculties) {
-            for(Department d: f.getDepartments()){
+
+    public Professor getProfessor(String name, String sname) {
+        for (Faculty f : faculties) {
+            for (Department d : f.getDepartments()) {
                 Professor p = d.findProfessor(name, sname);
-                if(p != null) {
+                if (p != null) {
                     return p;
                 }
             }
